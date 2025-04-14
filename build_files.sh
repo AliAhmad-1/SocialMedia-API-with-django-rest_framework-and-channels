5522c0b7e451755b9477808b1a0015a37bd0237e
Ali Ahmad <92890445+AliAhmad-1@users.noreply.github.com>
2025-04-14 17:03:47 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -5,4 +5,4 @@ curl https://bootstrap.pypa.io/get-pip.py | python3.9
 
 # Install project dependencies
 python3.9 -m pip install -r requirements.txt
-web: daphne backend.asgi:application -b 0.0.0.0 -p $PORT
+daphne backend.asgi:application -b 0.0.0.0 -p $PORT
