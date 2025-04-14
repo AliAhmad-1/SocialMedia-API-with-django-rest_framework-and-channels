c8cf13e42428686feebdf51c6df1e680b6beb008
Ali Ahmad <92890445+AliAhmad-1@users.noreply.github.com>
2025-04-14 20:35:06 +0300

    Update asgi.py

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,15 +1,17 @@
-from channels.routing import URLRouter,ProtocolTypeRouter
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+import django
+django.setup()
+
+
+
+from channels.routing import URLRouter,ProtocolTypeRouter
 from app.routing import websocket_urlpatterns
 from django.core.asgi import get_asgi_application
-
 from .middleware import JWTAuthMiddleware
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
 
 application = ProtocolTypeRouter({
 "http":get_asgi_application(),
 "websocket":
         JWTAuthMiddleware(URLRouter(websocket_urlpatterns))
     })
-app=application
